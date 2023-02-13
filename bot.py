f3012fd0bdad92b7fd73b5471cbcc53665e9423a
K4kug3n <kakugen@yahoo.fr>
2023-02-13 23:10:03 +0100

    Cleanup

--- bot.py
+++ bot.py
@@ -1,60 +1,35 @@
 import asyncio
 import discord
+from discord.ext import commands
 
 import logger
-from discord_utils import * 
-from config import *
-from database import *
-from utils import *
-
-import modules.kofi_module as Kofi
-
-from discord.ext import tasks, commands
+from config import Config
 
 config = Config()
 config.load_json('configs/config.json')
 
-database = Database(config.get('database_url'), 'Cluster0')
-
-kofi_config = Config()
-kofi_config.load_json('configs/kofi_config.json')
-if kofi_config.get('active') and not Kofi.init(kofi_config, database):
-	logger.fail_message("Can't init kofi module")
-	exit()
-
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-class Looper(commands.Cog):
-	def __init__(self, bot, kofi_config, database):
-		self.bot = bot
-		self.kofi_config = kofi_config
-		self.database = database
-
-		if kofi_config.get('active'):
-			self.update_kofi_loop.start()
-
-	def cog_unload(self):
-		self.update_kofi_loop.cancel()
-
-	@tasks.loop(minutes=10.0)
-	async def update_kofi_loop(self):
-		await Kofi.update(self.bot, self.kofi_config, self.database)
-
-	@update_kofi_loop.before_loop
-	async def kofi_before_bot(self):
-		await self.bot.wait_until_ready()
-
-@client.event
+@bot.event
 async def on_ready():
-	logger.success_message("""Logged in as {0.name} ({0.id})""".format(client.user))
+	logger.success_message("""Logged in as {0.name} ({0.id})""".format(bot.user))
 
-	Looper(client, kofi_config, database)
+@bot.command()
+@commands.has_permissions(administrator = True)
+async def list(ctx):
+	embed = discord.Embed(
+		title='Active module',
+		description='\n'.join(config.get("active_modules")),
+		color=0xFF5733
+	)
+
+	await ctx.send(embed=embed)
 
 async def main():
-	async with client:
+	async with bot:
 		for module in config.get("active_modules"):
-			await client.load_extension(f"""modules.{module}""")
-		await client.start(config.get('token'))
+			await bot.load_extension(f"""modules.{module}""")
+		await bot.start(config.get('token'))
 
 asyncio.run(main())
