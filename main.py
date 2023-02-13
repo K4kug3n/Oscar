81c77cfc9ccf44459078ec7ec0c3945fd3a2b35f
K4kug3n <maxime.rambosson@gmail.com>
2023-02-14 00:14:39 +0100

    Add module module

--- main.py
+++ main.py
@@ -15,19 +15,9 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
 	logger.success_message("""Logged in as {0.name} ({0.id})""".format(bot.user))
 
-@bot.command()
-@commands.has_permissions(administrator = True)
-async def list(ctx):
-	embed = discord.Embed(
-		title='Active module',
-		description='\n'.join(config.get("active_modules")),
-		color=0xFF5733
-	)
-
-	await ctx.send(embed=embed)
-
 async def main():
 	async with bot:
+		await bot.load_extension(f"""modules.module""")
 		for module in config.get("active_modules"):
 			await bot.load_extension(f"""modules.{module}""")
 		await bot.start(config.get('token'))
